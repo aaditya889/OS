00c7e5643651b83460af63e5a8fb93f25c6a9da7
Sahil Soni <SscSPs@gmail.com>
2019-10-23 12:56:39 +0530

    Organise the load_data.c file
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- Kernel/load_data.c
+++ Kernel/load_data.c
@@ -1,20 +1,24 @@
 #include"handlers.c"
 
+void load_data() {
+  char * codes,
+    let [50] = {
+      "QWERTYUIOP[]ASDFGHJKL;'`ZXCVBNM,./"
+    };
+  unsigned short x = 0;
+  codes = scan_codes;
+  for (int i = 16; i <= 27; i++)
+    codes[i] =
+    let [x++];
+  codes[28] = '\n';
+  codes[43] = '\\';
+  for (int i = 30; i <= 41; i++)
+    codes[i] =
+    let [x++];
 
-void load_data(){
-char *codes,let[50]={"QWERTYUIOP[]ASDFGHJKL;'`ZXCVBNM,./"};
-unsigned short x=0;
-codes = scan_codes;
-for(int i=16;i<=27;i++)
-codes[i]=let[x++];
-codes[28]='\n';
-codes[43]='\\';
-for(int i=30;i<=41;i++)
-codes[i]=let[x++];
+  for (int i = 44; i <= 53; i++)
+    codes[i] =
+    let [x++];
 
-for(int i=44;i<=53;i++)
-codes[i]=let[x++];
-
-codes[57]=' ';
+  codes[57] = ' ';
 }
-
